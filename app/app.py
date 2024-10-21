b5e730044f1bd4d056dbe3d6a36582ac31f4c18b
Iris <idbontekoe@hotmail.com>
2024-10-21 16:13:37 +0200

    Add example visualisation for testing

--- app/app.py
+++ app/app.py
@@ -2,9 +2,10 @@ from sdk.moveapps_spec import hook_impl
 from sdk.moveapps_io import MoveAppsIo
 from movingpandas import TrajectoryCollection
 import logging
+import matplotlib.pyplot as plt
 
-# This is a test to see if it works with PyCharm
-# Another test
+# import the function get_GDF from the file "./app/getGeoDataFrame.py"
+from app.getGeoDataFrame import get_GDF 
 
 class App(object):
 
@@ -13,12 +14,44 @@ class App(object):
 
     @hook_impl
     def execute(self, data: TrajectoryCollection, config: dict) -> TrajectoryCollection:
-        """Your app code goes here"""
+        
         logging.info(f'Welcome to the {config}')
 
+        """Your app code goes here"""
+        
+        # showcase injecting App settings (parameter `year`)
+        data_gdf = get_GDF(data) # translate the TrajectoryCollection to a GeoDataFrame
+        logging.info(f'Subsetting data for {config["year"]}')
+        # subset the data to only contain the specified year
+        if config["year"] in data_gdf.index.year:
+            result = data_gdf[data_gdf.index.year==config["year"]]
+        else:
+            result = None
+        
+        # showcase creating an artifact
+        if result is not None:
+            result.plot(column=data.get_traj_id_col())
+            plot_file = self.moveapps_io.create_artifacts_file("plot.png")
+            plt.savefig(plot_file)
+            logging.info(f'saved plot to {plot_file}')
+        else:
+            logging.warning("Nothing to plot")
+        
+        # showcase accessing auxiliary files
         auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
         with open(auxiliary_file_a, 'r') as f:
             logging.info(f.read())
+            
+        # Translate the result back to a TrajectoryCollection
+        if result is not None:
+            result = TrajectoryCollection(
+                result, 
+                traj_id_col=data.get_traj_id_col(),
+                t=data.to_point_gdf().index.name,
+                crs=data.get_crs()
+                )
+        else:
+            result = data
 
-        # return some useful data for next apps in the workflow
-        return data
+        # return the resulting data for next Apps in the Workflow
+        return result
