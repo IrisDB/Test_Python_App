7a33433b99f44b4e5fe4e94ac6b2c5b46e5f9e8f
Iris <idbontekoe@hotmail.com>
2024-11-06 16:39:53 +0100

    Add and test example

--- app/app.py
+++ app/app.py
@@ -5,29 +5,46 @@ import logging
 import matplotlib.pyplot as plt
 
 # import the function get_GDF from the file "./app/getGeoDataFrame.py"
-from app.getGeoDataFrame import get_GDF 
+from app.getGeoDataFrame import get_GDF
+
+# showcase injecting App settings (parameter `year`)
+from dataclasses import dataclass
+
+
+@dataclass
+class AppConfig:
+    year: int
+
 
 class App(object):
 
     def __init__(self, moveapps_io):
         self.moveapps_io = moveapps_io
 
+    # showcase injecting App settings (parameter `year`)
+    @staticmethod
+    def map_config(config: dict):
+        return AppConfig(
+            year=config["year"] if "year" in config else 1994
+        )
+
     @hook_impl
     def execute(self, data: TrajectoryCollection, config: dict) -> TrajectoryCollection:
-        
+
         logging.info(f'Welcome to the {config}')
 
         """Your app code goes here"""
-        
+
         # showcase injecting App settings (parameter `year`)
-        data_gdf = get_GDF(data) # translate the TrajectoryCollection to a GeoDataFrame
-        logging.info(f'Subsetting data for {config["year"]}')
+        app_config = self.map_config(config=config)
+        data_gdf = get_GDF(data)  # translate the TrajectoryCollection to a GeoDataFrame
+        logging.info(f'Subsetting data for {app_config.year}')
         # subset the data to only contain the specified year
-        if config["year"] in data_gdf.index.year:
-            result = data_gdf[data_gdf.index.year==config["year"]]
+        if app_config.year in data_gdf.index.year:
+            result = data_gdf[data_gdf.index.year == app_config.year]
         else:
             result = None
-        
+
         # showcase creating an artifact
         if result is not None:
             result.plot(column=data.get_traj_id_col())
@@ -36,22 +53,20 @@ class App(object):
             logging.info(f'saved plot to {plot_file}')
         else:
             logging.warning("Nothing to plot")
-        
+
         # showcase accessing auxiliary files
-        auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("aux_id_A")
-        with open(auxiliary_file_a, 'r') as f:
-            logging.info(f.read())
-            
+        # auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
+        # with open(auxiliary_file_a, 'r') as f:
+        #    logging.info(f.read())
+
         # Translate the result back to a TrajectoryCollection
         if result is not None:
             result = TrajectoryCollection(
-                result, 
+                result,
                 traj_id_col=data.get_traj_id_col(),
                 t=data.to_point_gdf().index.name,
                 crs=data.get_crs()
-                )
-        else:
-            result = data
-
+            )
+        logging.info(result)
         # return the resulting data for next Apps in the Workflow
         return result
